d3f6925a8961602cfab296bff645363657925483
Ivan Razumov <stdbtt@gmail.com>
2022-06-15 17:23:32 +0300

    create validator for unique email

--- crud_app/src/main/java/stdbtt/springcourse/controllers/PeopleController.java
+++ crud_app/src/main/java/stdbtt/springcourse/controllers/PeopleController.java
@@ -1,6 +1,5 @@
 package stdbtt.springcourse.controllers;
 
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,16 +7,21 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import stdbtt.springcourse.dao.PersonDAO;
 import stdbtt.springcourse.models.Person;
+import stdbtt.springcourse.util.PersonValidator;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
     private final PersonDAO personDAO;
+    private final PersonValidator personValidator;
 
     @Autowired
-    public PeopleController(PersonDAO personDAO) {
+    public PeopleController(PersonDAO personDAO, PersonValidator personValidator) {
         this.personDAO = personDAO;
+        this.personValidator = personValidator;
     }
 
     @GetMapping()
@@ -40,6 +44,8 @@ public class PeopleController {
 
     @PostMapping()
     public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult){
+        personValidator.validate(person, bindingResult);
+
         if(bindingResult.hasErrors()){
             return "people/new";
         }
@@ -55,7 +61,7 @@ public class PeopleController {
 
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult, @PathVariable("id") int id){
-
+        personValidator.validate(person, bindingResult);
         if (bindingResult.hasErrors()){
            return "people/edit";
         }
--- crud_app/src/main/java/stdbtt/springcourse/dao/PersonDAO.java
+++ crud_app/src/main/java/stdbtt/springcourse/dao/PersonDAO.java
@@ -10,6 +10,7 @@ import stdbtt.springcourse.models.Person;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class PersonDAO {
@@ -28,6 +29,11 @@ public class PersonDAO {
     public Person show(int id){
         return jdbcTemplate.query("SELECT * FROM person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
                 .stream().findAny().orElse(null);
+    }
+
+    public Optional<Person> show(String email){
+        return jdbcTemplate.query("SELECT * FROM person WHERE email=?", new Object[]{email}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny();
     }
 
     public void save(Person person){
