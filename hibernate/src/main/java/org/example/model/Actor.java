090b4e5de367ecbca851f9fe99f584d73bfa3c86
Ivan Razumov <stdbtt@gmail.com>
2022-06-23 16:17:54 +0300

    add Actor and Movie

---
+++ hibernate/src/main/java/org/example/model/Actor.java
@@ -0,0 +1,108 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "actor")
+public class Actor {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    @ManyToMany
+    @JoinTable(
+            name = "actor_movie",
+            joinColumns = @JoinColumn (name = "actor_id"),
+            inverseJoinColumns = @JoinColumn (name  = "movie_id")
+    )
+    private List<Movie> movies;
+
+    public Actor() {
+    }
+
+    public Actor(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder builder = new StringBuilder();
+        for (Movie m:
+             movies) {
+            builder.append(m.getName()).append(", ");
+        }
+
+        return "Actor{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", movies=" + builder +
+                '}';
+    }
+
+    public void addMovie(Movie movie){
+        if(movies == null)
+            movies = new ArrayList<>();
+        movies.add(movie);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Actor actor = (Actor) o;
+        return id == actor.id && age == actor.age && Objects.equals(name, actor.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
+    }
+}
