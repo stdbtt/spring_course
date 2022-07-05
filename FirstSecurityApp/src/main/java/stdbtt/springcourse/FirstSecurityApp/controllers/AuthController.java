665bc531b97ff0bb36cb6df7736fe544cd55e7eb
Ivan Razumov <stdbtt@gmail.com>
2022-07-05 15:48:23 +0300

    add logout

--- FirstSecurityApp/src/main/java/stdbtt/springcourse/FirstSecurityApp/controllers/AuthController.java
+++ FirstSecurityApp/src/main/java/stdbtt/springcourse/FirstSecurityApp/controllers/AuthController.java
@@ -1,6 +1,8 @@
 package stdbtt.springcourse.FirstSecurityApp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import stdbtt.springcourse.FirstSecurityApp.models.Person;
+import stdbtt.springcourse.FirstSecurityApp.security.PersonDetails;
 import stdbtt.springcourse.FirstSecurityApp.services.RegistrationService;
 import stdbtt.springcourse.FirstSecurityApp.util.PersonValidator;
 
