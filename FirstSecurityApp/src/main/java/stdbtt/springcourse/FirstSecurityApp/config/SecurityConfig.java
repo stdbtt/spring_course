48429f927d1611f34292f891c11eea2d69f67dab
Ivan Razumov <stdbtt@gmail.com>
2022-07-05 14:23:44 +0300

    edit auth page

--- FirstSecurityApp/src/main/java/stdbtt/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ FirstSecurityApp/src/main/java/stdbtt/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package stdbtt.springcourse.FirstSecurityApp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,6 +20,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.personDetailsService = personDetailsService;
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        //конфигуририруем сам Spring Security
+        //конфигурируем авториазацию
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello",true)
+                .failureUrl("/auth/login?error");
+    }
+
+    //НАстраиваем аутентификацию
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
     }
